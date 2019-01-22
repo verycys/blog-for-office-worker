ed9ae06e5f780029ba49d5ba5c163ab09818c846
Yunseok Choi <yunseokchoi@Yunseokui-MacBook-Pro.local>
2019-01-22 21:39:06 +0900

    Set default, Add mig and admin settings and Deco with bootstrap

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Article, Comment, HashTag
 
 # Create your views here.
+def index(request):
+    article_list = Article.objects.all()
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
+
+def detail(request):
+    pass
+
+# def about(request):
+#     pass
