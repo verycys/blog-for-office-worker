0ecc984f47e3465db739f3e09040719b9344a0e9
Yunseok Choi <yunseokchoi@Yunseokui-MacBook-Pro.local>
2019-01-24 15:34:27 +0900

    Add comments on detail.html

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Article, Comment, HashTag
 
 # Create your views here.
 def index(request):
-    # GET $ POST
+    # GET & POST
     category = request.GET.get("category")
     hashtag = request.GET.get("hashtag")
 
@@ -36,12 +37,33 @@ def index(request):
     return render(request, "index.html", ctx)
 
 def detail(request, article_id):
+    # GET & POST
+
     article = Article.objects.get(id=article_id)
+    # comment_list = Comment.objects.filter(article__id=article_id)
+    # comment_list = article.article.comments.all()
     hashtag_list = HashTag.objects.all()
     ctx = {
         "article" : article,
+        # "comment_list" : comment_list,
         "hashtag_list" : hashtag_list,
     }
+
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        content = request.POST.get("content")
+        # print(username)
+        # print(content)
+        Comment.objects.create(
+            article=article,
+            username=username,
+            content=content
+        )
+
+        return HttpResponseRedirect("/{}/".format(article_id))
+
     return render(request, "detail.html", ctx)
 
 # def about(request):
